067027f46f9a3e97a301153fdc07ab57a50b9887
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-01-10 09:37:56 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-
-function tocarPom() {
-	document.querySelector("#som_tecla_pom").play();
+function tocarSomPom() {
+	document.querySelector('#som_tecla_pom').play()
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocarSomPom;
+listaDeTeclas[1].onclick = tocarSomClap;
+listaDeTeclas[2].onclick = tocarSomTim;
+listaDeTeclas[3].onclick = tocarSomPuff;
+listaDeTeclas[4].onclick = tocarSomSplash;
+listaDeTeclas[5].onclick = tocarSomToim;
+listaDeTeclas[6].onclick = tocarSomPsh;
+listaDeTeclas[7].onclick = tocarSomTic;
+listaDeTeclas[8].onclick = tocarSomTom;
+
