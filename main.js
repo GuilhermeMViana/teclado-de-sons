c40ce0bd64dd11c669af663f630b636414577334
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-01-11 10:05:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function tocarSomPom() {
-	document.querySelector('#som_tecla_pom').play()
+function tocarSom(idAudio) {
+	document.querySelector(idAudio).play()
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocarSomPom;
-listaDeTeclas[1].onclick = tocarSomClap;
-listaDeTeclas[2].onclick = tocarSomTim;
-listaDeTeclas[3].onclick = tocarSomPuff;
-listaDeTeclas[4].onclick = tocarSomSplash;
-listaDeTeclas[5].onclick = tocarSomToim;
-listaDeTeclas[6].onclick = tocarSomPsh;
-listaDeTeclas[7].onclick = tocarSomTic;
-listaDeTeclas[8].onclick = tocarSomTom;
+for (let i = 0; i < listaDeTeclas.length; i++) {
 
+	const tecla = listaDeTeclas[i]
+	const instrumento = tecla.classList[1]
+	const idAudio = `#som_${instrumento}`
+
+	tecla.onclick = function () {
+		tocarSom(idAudio)
+	}
+
+}
