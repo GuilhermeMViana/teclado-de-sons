49c983ea5978fe75b78cbb4b5daacea1e8093a97
Guilherme Viana <97542638+GuilhermeMViana@users.noreply.github.com>
2023-01-12 16:58:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-function tocarSom(idAudio) {
-	document.querySelector(idAudio).play()
+function tocarSom(seletorAudio) {
+	const elemento = document.querySelector(seletorAudio).play()
+
+	if (elemento === null) {
+		console.log('Elemento não encontrado.')
+	} else {
+
+		if (elemento.localName === 'Audio')
+			elemento.play()
+	}
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
 
 	const tecla = listaDeTeclas[i]
 	const instrumento = tecla.classList[1]
-	const idAudio = `#som_${instrumento}`
+	const seletorAudio = `#som_${instrumento}`
 
 	tecla.onclick = function () {
-		tocarSom(idAudio)
+		tocarSom(seletorAudio)
 	}
-	/*  Teste
-	   if (keycode === 13 || keycode === 32) {
-   
-		   tecla.onkeydown = function () {
-			   tecla.classList.add('ativa');
-		   }
-   
-		   tecla.onkeyup = function () {
-			   tecla.classList.remove('ativa');
-		   }
-	   }
-   */
+
+	tecla.onkeydown = function (event) {
+
+		if (event.code === 'Enter' || event.code === 'Space') {
+			tecla.classList.add('ativa');
+		}
+
+	}
+
+	tecla.onkeyup = function () {
+		tecla.classList.remove('ativa');
+	}
 
 }
